2520f086e1fd152e125d3d4f452911c81d90873b
phoenix418 <151996009+phoenix418@users.noreply.github.com>
2024-11-28 20:16:53 +0700

    Update основная часть кода.cpp

--- основная часть кода.cpp
+++ основная часть кода.cpp
@@ -2,30 +2,181 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <thread>
 using namespace std;
 
 
-class MenuItem{
+void  drawMenu(){
+  for (int i = 0; i < 1; i++) {
+    cout << "\x1B[2J\x1B[H";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "                      *************              " << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                     " << endl;
+    cout << "                     " << endl;
+    cout << "                     " << endl;
+    cout << "                      *************              " << endl;
+    chrono::milliseconds search2(1000);
+    this_thread::sleep_for(search2);
+    cout << "\x1B[2J\x1B[H";
+
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "                      *************              " << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                    " << endl;
+    cout << "                    " << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                      *************              " << endl;
+    this_thread::sleep_for(search2);
+    cout << "\x1B[2J\x1B[H";
+
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "                      *************              " << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                     |   запуск    |" << endl;
+    cout << "                     |   системы   |" << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                      *************              " << endl;
+    this_thread::sleep_for(search2);
+    cout << "\x1B[2J\x1B[H";
+  }
+  for (int i = 0; i < 3; i++) {
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "      " << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                     |   запуск    |" << endl;
+    cout << "                     |   системы   |" << endl;
+    cout << "                     |=============|" << endl;
+    cout << "      " << endl;
+    chrono::milliseconds serch(100);
+    this_thread::sleep_for(serch);
+    cout << "\x1B[2J\x1B[H";
+
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "                      **  " << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                     |   запуск    |" << endl;
+    cout << "                     |   системы   |" << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                      **  " << endl;
+    this_thread::sleep_for(serch);
+    cout << "\x1B[2J\x1B[H";
+
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "                      ****  " << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                     |   запуск    |" << endl;
+    cout << "                     |   системы   |" << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                      ****  " << endl;
+    this_thread::sleep_for(serch);
+    cout << "\x1B[2J\x1B[H";
+
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "                      ******  " << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                     |   запуск    |" << endl;
+    cout << "                     |   системы   |" << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                      ******  " << endl;
+    this_thread::sleep_for(serch);
+    cout << "\x1B[2J\x1B[H";
+
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "                      ********  " << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                     |   запуск    |" << endl;
+    cout << "                     |   системы   |" << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                      ********  " << endl;
+    this_thread::sleep_for(serch);
+    cout << "\x1B[2J\x1B[H";
+
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "                      **********  " << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                     |   запуск    |" << endl;
+    cout << "                     |   системы   |" << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                      **********  " << endl;
+    this_thread::sleep_for(serch);
+    cout << "\x1B[2J\x1B[H";
+
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "                      *************  " << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                     |   запуск    |" << endl;
+    cout << "                     |   системы   |" << endl;
+    cout << "                     |=============|" << endl;
+    cout << "                      *************  " << endl;
+    this_thread::sleep_for(serch);
+    cout << "\x1B[2J\x1B[H";
+  }
+}
+
+
+class MenuItem {
 private:
     string name;
     double price;
     string ingredients;
 
 public:
-    MenuItem(string n, double p, string ing) {
-        name = n;
-        price = p;
-        ingredients = ing;
-    }
+    MenuItem(string n, double p, string ing) : name(n), price(p), ingredients(ing) {}
 
 
     void displayInfo() {
-        cout << name << " - " << price << " (Состав: " << ingredients << ")"<< endl;
+        cout << name << " - " << price << " (Состав: " << ingredients << ")" << endl;
     }
 
 
     double getPrice() {
         return price;
+    }
+
+
+    string getName() {
+        return name;
     }
 };
 
@@ -37,11 +188,8 @@ private:
     double totalPrice;
 
 public:
-    Order(int id) {
-        orderID = id;
-        totalPrice = 0.0;
-    }
-    
+    Order(int id) : orderID(id), totalPrice(0.0) {}
+
 
     void addItem(MenuItem item) {
         items.push_back(item);
@@ -49,13 +197,31 @@ public:
     }
 
 
-    void displayOrder(){
+    void removeItem(int index) {
+        if (index >= 0 && index < items.size()) {
+            totalPrice -= items[index].getPrice();
+            items.erase(items.begin() + index);
+        }
+    }
+
+
+    void displayOrder() {
         cout << "Заказ ID: " << orderID << endl;
         cout << "Содержимое заказа:" << endl;
         for (int i = 0; i < items.size(); i++) {
             items[i].displayInfo();
         }
         cout << "Общая стоимость: " << totalPrice << endl;
+    }
+
+
+    int getItemCount() {
+        return items.size();
+    }
+
+
+    MenuItem getItem(int index) {
+        return items[index];
     }
 };
 
@@ -65,18 +231,21 @@ private:
     string name;
 
 public:
-    Customer(string n){
-        name = n;
+    Customer(string n) : name(n) {}
+
+
+    void displayInfo() {
+        cout << "Клиент: " << name << endl;
     }
 
 
-    void displayInfo(){
-        cout << "Клиент: " << name << endl;
+    string getName() {
+        return name;
     }
 };
 
 
-class Restaurant{
+class Restaurant {
 private:
     vector<MenuItem> menu;
     vector<Order> orders;
@@ -121,20 +290,61 @@ public:
             cin >> itemIndex;
             if (itemIndex >= 0 && itemIndex < menu.size()) {
                 order.addItem(menu[itemIndex]);
-            } 
-            else if (itemIndex != -1) {
+                cout << "\x1B[2J\x1B[H";
+            } else if (itemIndex != -1) {
                 cout << "Неверный номер блюда. Попробуйте снова." << endl;
+                cout << "\x1B[2J\x1B[H";
             }
-        } 
-        while (itemIndex != -1);
-        orders.push_back(order);
+        } while (itemIndex != -1);
+
         order.displayOrder();
+        handleOrderModification(order);
+        orders.push_back(order);
     }
 
 
-    void displayMenu(){
+    void handleOrderModification(Order& order) {
+        char choice;
+        do {
+            cout << "Хотите изменить заказ? (y/n): ";
+            cin >> choice;
+            if (choice == 'y') {
+                int action;
+                cout << "1. Добавить блюдо" << endl;
+                cout << "2. Удалить блюдо" << endl;
+                cout << "Выберите действие: ";
+                cin >> action;
+                if (action == 1) {
+                    displayMenu();
+                    int itemIndex;
+                    cout << "Выберите номер блюда для добавления: ";
+                    cin >> itemIndex;
+                    if (itemIndex >= 0 && itemIndex < menu.size()) {
+                        order.addItem(menu[itemIndex]);
+                    }
+                } 
+                else if (action == 2) {
+                    if (order.getItemCount() > 0) {
+                        order.displayOrder();
+                        int removeIndex;
+                        cout << "Введите номер блюда для удаления: ";
+                        cin >> removeIndex;
+                        order.removeItem(removeIndex);
+                    } 
+                    else {
+                        cout << "В заказе нет блюд для удаления." << endl;
+                    }
+                }
+                order.displayOrder();
+            }
+        }
+            
+    }
+
+
+    void displayMenu() {
         cout << "Меню:" << endl;
-        for (int i= 0; i < menu.size(); i++){
+        for (int i = 0; i < menu.size(); i++) {
             cout << i << ". ";
             menu[i].displayInfo();
         }
@@ -143,11 +353,13 @@ public:
 
 
 int main() {
+    drawMenu();
     Restaurant restaurant;
     restaurant.loadMenu("menu.txt");
     string customerName;
     cout << "Введите имя клиента: ";
     getline(cin, customerName);
+    cout << "\x1B[2J\x1B[H";
     Customer customer(customerName);
     restaurant.createOrder(customer);
     return 0;
