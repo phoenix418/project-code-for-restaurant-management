14659ed654e54e0e6cfa03b3bdf3e23ec1d2a34a
phoenix418 <151996009+phoenix418@users.noreply.github.com>
2024-12-09 22:29:06 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <thread>
+#include "draw.cpp" 
+#include "MenuItem.h"
+#include "Order.h"
+#include "Customer.h"  
+#include "Restaurant.h" 
+using namespace std;
+
+
+
+#endif main() {
+    drawMenu();
+    Restaurant restaurant;
+    restaurant.loadMenu("menu.txt");
+    string customerName;
+    cout << "Введите имя клиента: ";
+    getline(cin, customerName);
+    cout << "\x1B[2J\x1B[H";
+    Customer customer(customerName);
+    restaurant.createOrder(customer);
+    return 0;
+}
