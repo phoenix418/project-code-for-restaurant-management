7ebcbae5d46c5189a0fe5166edceff1d87490506
phoenix418 <151996009+phoenix418@users.noreply.github.com>
2024-12-17 17:50:42 +0700

    Create draw.h

---
+++ draw.h
@@ -0,0 +1,11 @@
+#ifndef DRAW_H
+#define DRAW_H
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <thread>
+using namespace std;
+
+extern int drawMenu();
+#endif
