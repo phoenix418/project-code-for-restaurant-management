4662d0a66a28798507003e85526c8d3fb3126a33
phoenix418 <151996009+phoenix418@users.noreply.github.com>
2024-12-17 17:51:03 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,24 +3,24 @@
 #include <string>
 #include <fstream>
 #include <thread>
-#include "draw.cpp" 
 #include "MenuItem.h"
 #include "Order.h"
 #include "Customer.h"  
-#include "Restaurant.h" 
+#include "Restaurant.h"
+#include "draw.cpp"
 using namespace std;
 
 
-
-#endif main() {
-    drawMenu();
-    Restaurant restaurant;
-    restaurant.loadMenu("menu.txt");
-    string customerName;
-    cout << "Введите имя клиента: ";
-    getline(cin, customerName);
-    cout << "\x1B[2J\x1B[H";
-    Customer customer(customerName);
-    restaurant.createOrder(customer);
-    return 0;
+int main() {
+setlocale(LC_ALL, "rus");
+drawMenu();
+Restaurant restaurant;
+restaurant.loadMenu("menu.txt");
+string customerName;
+cout << "Введите имя клиента: ";
+getline(cin, customerName);
+cout << "\x1B[2J\x1B[H";
+Customer customer(customerName);
+restaurant.createOrder(customer);
+return 0;
 }
